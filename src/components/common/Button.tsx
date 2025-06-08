98a9d12002245744fa8e8ee627512fe1797200e4
kiraberser <121148745+kiraberser@users.noreply.github.com>
2025-06-08 11:59:31 -0600

    chore: inicializar estructura base del proyecto RosaMenta

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,10 @@
+
+const Button = () => {
+  return (
+    <button className="btn btn-outline btn-secondary btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl ">
+      Click Me
+    </button>
+  )
+}
+
+export default Button
